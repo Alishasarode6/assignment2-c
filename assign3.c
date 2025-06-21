3c5e6f2dee050f0b5cf7474bfb03c2c8fc707dba
Savita Sarode <sarodesavita64@gmail.com>
2025-06-21 21:10:47 +0530

    first commit

---
+++ assign3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number");
+    scanf("%d", &num);
+    if (num %2 ==0)
+    printf("enter a number is even");
+else
+    printf("enter a number is odd");
+
+return 0;
+}
